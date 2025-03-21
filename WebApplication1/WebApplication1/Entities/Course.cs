bde49cebd5b422203323eea523e007fdabc399a0
Mehrdad <mehrdad.mohajjel@gmail.com>
2025-03-22 01:32:57 +0330

    Add Constractor To Entities

---
+++ WebApplication1/WebApplication1/Entities/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Entities
+{
+    public class Course(long Id,string Name,string Description)
+    {
+        public long Id { get; set; } = Id;
+        public string Name { get; set; } = Name;
+        public string Description { get; set; } = Description;
+    }
+}
